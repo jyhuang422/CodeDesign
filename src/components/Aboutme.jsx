624f158683616811650347e450a1cad262746739
jyhuang <jyhuang422@gmail.com>
2017-02-19 19:30:32 +0800

    Add Aboutme svg and style change

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styles from './main.css'
 
 const aboutme = ({current}) => (
-  <div>
-    About me
+  <div className={styles.aboutmeMain}>
+    <img src="/dist/images/learning.svg" />
+    <div>Under Construction</div>
   </div>
 )
 
