e47c93178e5c2943fd51f1126d81bd74a5f80198
jyhuang <jyhuang422@gmail.com>
2017-04-04 02:14:07 +0800

    [test] change port to 80

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -61,7 +61,8 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production'),
+        PORT: JSON.stringify(80)
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
