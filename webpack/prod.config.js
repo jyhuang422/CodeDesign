e9c0bfaae225c461a883dad077303da01ba73c6d
jyhuang <jyhuang422@gmail.com>
2017-04-04 03:01:26 +0800

    [webpack] remove webpack define port

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -61,8 +61,7 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-        PORT: JSON.stringify(process.env.PORT || 3000)
+        NODE_ENV: JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
